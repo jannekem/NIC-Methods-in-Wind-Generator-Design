14b527f075c82952a513bf7193d7f13ed93e1c7f
Janne Kemppainen <kemppainen.janne@outlook.com>
2014-03-16 00:28:32 +0200

    PSO Start new implementation

--- PSO/PSO_.m
+++ PSO/PSO_.m
@@ -1,17 +1,19 @@
 %initialize parameters
 clear all
-t_end = 100;
-particleAmount = 50;
-parameterAmount = 2;
-limits = [-1 1; -1 1]; % parameter limits
-
+t_end = 15;
+particleAmount = 10;
+limits = [35000 65000; 2000000 6000000;
+    0.001 0.05; 0.8 5; 0.6 0.95; 21000 48000;
+    1.3 1.6; 0.8 0.99; 0.25 0.75;
+    0.75 0.95; 0.01 0.1; 0.01 0.1]; % parameter limits
+discrete_limits = [20 80; 1 3]; % indices for integer parameters
 
 % generate the swarm and velocity matrices
-swarm = zeros(t_end, particleAmount, parameterAmount);
+swarm = zeros(t_end, particleAmount, 12);
 swarmd = zeros(t_end, particleAmount, 2);
-velocity = zeros(particleAmount, parameterAmount);
+velocity = zeros(particleAmount, 12);
 velocityd = zeros(particleAmount, 2);
-bestpars = zeros(particleAmount, parameterAmount); % best parameters for each particle 
+bestpars = zeros(particleAmount, 12); % best parameters for each particle 
 bestparsd = zeros(particleAmount, 2); % best discrete parameters ""
 bestresults = zeros(particleAmount,1);
 bestidx = 0;        % holder for the global best index
@@ -19,26 +21,38 @@ bestval = 0;
 
 % initialize swarm
 for ind = 1:particleAmount 
-    for par = 1:parameterAmount
+    for par = 1:12
        swarm(1,ind,par) = limits(par,1)+rand()*(limits(par,2)-limits(par,1));
     end
+    swarmd(1,ind,1) = randi([20 80],1);
+    swarmd(1,ind,2) = randi([1 3],1);
 end
 
-for ind = 1:particleAmount
-    params = swarm(1,ind,:);
-    val = -swarm(1,ind,1)^2-swarm(1,ind,2)^2;
+parfor ind = 1:particleAmount
+    params = combineparams(swarm(1,ind,:), swarmd(1,ind,:));
+    [objects, constraints] = design_PMSM_generator(params);
     bestpars(ind,:) = swarm(1,ind,:);
     bestparsd(ind,:) = swarmd(1,ind,:);
-    bestresults(ind) = val;
+    bestresults(ind) = evalobjects(objects, constraints);
 end
 
 [bestval, bestidx] = max(bestresults);
+
+
+fig = figure('Visible','off');
+x = swarm(1,:,1);
+y = swarm(1,:,2);
+scatter(x,y);
+
+% save video frame
+aviobj = avifile('out.avi','FPS',1);
+img = hardcopy(fig, '-dzbuffer', '-r0'); 
+aviobj = addframe(aviobj, im2frame(img));
 
 paratemp = zeros(particleAmount, 14); % DEBUG VARIABLE FOR FINDING TIME INTENSIVE PARAMETER COMBINATIONS
 
     meas_t = zeros(t_end,1);
 for t=2:t_end
-    
     swarm_temp = squeeze(swarm(t-1,:,:)); % required for matrix subtraction
     % create parameter validity matrix to ignore bad personal bests
     validity = bestresults;
@@ -48,21 +62,12 @@ for t=2:t_end
         
     % calculate velocity
     
-%     velocity = velocity...
-%         + 2*rand(size(velocity)).*(bestpars-swarm_temp)...
-%         + 2*rand(size(velocity))...
-%         .*(ones(particleAmount,1)*bestpars(bestidx,:)-swarm_temp)
-    velocityLimitFraction = 0.01;
-    for ind = 1:particleAmount
-        for par = 1:parameterAmount
-            velocity(ind,par) = velocity(ind,par)+2*rand()*(bestpars(ind,par)-swarm_temp(ind,par))+2*rand()*(bestpars(bestidx,par)-swarm_temp(ind,par));
-            % limit velocity
-            velocity(ind,par) = max(min(velocity(ind,par),velocityLimitFraction*(limits(par,2)-limits(par,1))),velocityLimitFraction*(limits(par,1)-limits(par,2)));
-        end
-    end
-
-    % update swarm locations
+    velocity = velocity...
+        + 2*rand(size(velocity)).*(bestpars-swarm_temp)...
+        + 2*rand(size(velocity))...
+        .*(ones(particleAmount,1)*bestpars(bestidx,:)-swarm_temp);
     
+    % update swarm locations
     swarm(t,:,:) = swarm_temp + velocity;
     
     % check boundaries
@@ -72,10 +77,11 @@ for t=2:t_end
     %boundcheck(boundcheck~=0) = 1;
  
     % simulate
-    for ind = 1:particleAmount 
+    parfor ind = 1:particleAmount 
         if size(boundcheck(ind)==0) % check that none of the values is over limits
-            params = swarm(t,ind,:); % TODO: discrete value handling
-            result = -swarm(t,ind,1)^2-swarm(t,ind,2)^2;
+            params = combineparams(swarm(t,ind,:), swarmd(1,ind,:)); % TODO: discrete value handling
+            [objects, constraints] = design_PMSM_generator(params);
+            result = evalobjects(objects, constraints);
         else
             result = -200;
         end
@@ -92,32 +98,16 @@ for t=2:t_end
        bestval = bestvalcand;
        bestid = bestidxcand;
     end
-    
-   
-    
+    x = swarm(t,:,1);
+    y = swarm(t,:,2);
+    scatter(x,y);
+    % save to video
+    img = hardcopy(fig, '-dzbuffer', '-r0'); 
+    aviobj = addframe(aviobj, im2frame(img));
     t
 end
 
-% % display figure
-hf = figure('color','white');
-hold on
-x = swarm(1,:,1);
-y = swarm(1,:,2);
-ht = scatter(x,y);
-set(ht,'XDataSource','x')
-set(ht,'YDataSource','y')
-drawnow
+params = combineparams(bestpars(bestidx,:)', bestparsd(bestidx,:));
+[objects, constraints] = design_PMSM_generator(params)
 
-for t = 1:t_end
-    pause(0.01)
-    x = swarm(t,:,1);
-    y = swarm(t,:,2);
-    refreshdata(hf,'caller');
-    axis([limits(1,1) limits(1,2) limits(2,1) limits(2,2)])
-    drawnow
-end
-
-
-%params = combineparams(bestpars(bestidx,:)', bestparsd(bestidx,:));
-%[objects, constraints] = design_PMSM_generator(params)
-
+aviobj = close(aviobj);

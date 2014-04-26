5074dd9eedf9ae6c87d72c7d4637d1e466d0d03c
Janne Kemppainen <kemppainen.janne@outlook.com>
2014-04-26 18:03:59 +0300

    Added sbatch scripts

---
+++ PSO/PSO_SLURM/matslurm_random_inertia.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --time=0-00:05:00 --mem-per-cpu=500
+#SBATCH -p play
+#SBATCH -o job-%a.out
+#SBATCH  --array=1-100
+
+# define the working dir on the compute node
+node_dir="local/$USER"
+
+# check if it doesn't exist and create one
+[ -d $node_dir ] || mkdir $node_dir
+
+module load matlab
+matlab -nojvm -r "PSO_function_random_inertia($SLURM_ARRAY_TASK_ID,50,500,50)"

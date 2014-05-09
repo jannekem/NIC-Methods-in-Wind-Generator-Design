0c92a7b6fc92714336f1d6346cf5833c3de7feea
Janne Kemppainen <kemppainen.janne@outlook.com>
2014-05-09 18:22:42 +0300

    Modified data analysis

--- PSO/Calculation Data/PSO-1/analyze_data.m
+++ PSO/Calculation Data/PSO-1/analyze_data.m
@@ -1,6 +1,9 @@
 clear all
-bestvalues=[];
-parameters=[];
+bestvalues=[];  % best value of each run
+parameters=[];  % best parameters of each run
+
+values=[];      % the development of the values
+
 iterations = 1000;
 failures = 0;
 
@@ -11,6 +14,7 @@ for index = 1:iterations
         load ( filename );
         parameters = [ parameters; params ];
         bestvalues = [ bestvalues, bestval ];
+        values = [values, bestvals];
     catch
         disp ( sprintf ( 'FAILURE no file %s', filename ) );
         failures = failures + 1;
@@ -20,5 +24,35 @@ for index = 1:iterations
 end
 mean_value = mean ( bestvalues )
 variance = var ( bestvalues )
-hist ( bestvalues )
+%hist ( bestvalues )
 
+%% (ii) Compute mean consumption
+M = length(bestvalues);
+N = length(bestvals);
+stdv=zeros(1,N);
+mv = zeros(1,N);
+for indn = 1:N
+    mv(indn)=sum(values(indn,:))/M;
+    stdv(indn)=std(values(indn,:));
+end
+
+
+figure(2);
+% create custom grayscale colormap
+customColormap=[];
+for ind = 1:70
+    val = 250 - ind*1;
+    customColormap = [customColormap; val,val,val];
+end
+customColormap = customColormap./255;
+
+
+set(gca,'NextPlot','replacechildren','ColorOrder',customColormap)
+plot(values);
+hold on;
+plot(mv,'r','Linewidth',4,'Color', 'b');
+plot(mv+stdv,'k','Linewidth',2.5);
+plot(mv-stdv,'k','Linewidth',2.5);
+hold off;
+xlabel('iteration');ylabel('efficiency');
+%axis([0 10 0 30])
